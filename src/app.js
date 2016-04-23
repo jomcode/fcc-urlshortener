1a42a3a7283b2ae2162ae01c4d68a3ac2b60082f
John <jomcode@gmail.com>
2016-04-23 05:42:48 -0500

    user story requirements working locally

--- src/app.js
+++ src/app.js
@@ -1,22 +1,45 @@
 const express = require('express');
+const validator = require('validator');
 
-const storage = {};
+function MemoryStorage() {
+  this.data = [];
+}
 
-const createShortenedUrl = () => {};
+MemoryStorage.prototype.set = function create(original) {
+  const id = this.data.length;
+  this.data.push(original);
+  return id;
+};
 
-const getOriginalUrl = () => {};
+MemoryStorage.prototype.get = function get(id) {
+  return this.data[id];
+};
+
+const createShortenedUrl = (storage, originalUrl) => validator.isURL(originalUrl) ?
+  storage.set(originalUrl) :
+  null;
+
+const getOriginalUrl = (storage, id) => storage.get(Number(id)) === undefined ?
+  null :
+  storage.get(id);
 
 const app = express();
 
+const storage = new MemoryStorage();
+
 app.get('/:id', (req, res) => {
-  console.log(`/${req.params.id}`);
-  res.json({ foo: 'foo' });
+  const id = req.params.id;
+  const target = getOriginalUrl(storage, id);
+  return target === null ? res.json({ error: 'invalid id' }) : res.redirect(target);
 });
 
-app.get('/new/:originalUrl', (req, res) => {
-  const originalUrl = req.params.originalUrl;
-  console.log(`/new/${originalUrl}`);
-  res.json({ bar: 'bar' });
+app.get('/new/*', (req, res) => {
+  const originalUrl = req.params[0];
+  const id = createShortenedUrl(storage, originalUrl);
+  const shortenedUrl = `${req.protocol}://${req.get('host')}/${id}`;
+  return id === null ?
+    res.json({ error: 'invalid url' }) :
+    res.json({ originalUrl, shortenedUrl });
 });
 
 module.exports.app = app;
